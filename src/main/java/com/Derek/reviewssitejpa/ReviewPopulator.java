ce86b0ee99f0fb0223561ad1b25a4da70480cd90
Derek <daanderso@gmail.com>
2017-07-04 23:10:49 -0400

    updates

--- src/main/java/com/Derek/reviewssitejpa/ReviewPopulator.java
+++ src/main/java/com/Derek/reviewssitejpa/ReviewPopulator.java
@@ -19,10 +19,13 @@ public class ReviewPopulator implements CommandLineRunner {
 		//creating genre objects
 		Genre action = new Genre("Action");
 		Genre drama = new Genre ("Drama");
+		Genre comedy = new Genre ("Comedy");
 		
 		//saving genre objects to genre repo
 		genreRepo.save(action);
 		genreRepo.save(drama);
+		genreRepo.save(comedy);
+		
 		
 		//creating review objects
 		Review review1 = new Review(action, "Batman",
@@ -77,16 +80,31 @@ public class ReviewPopulator implements CommandLineRunner {
 						+ "\r\n"
 						+ "Mel prima laudem at. Sed ut quodsi prompta facilisis, cum ad congue iisque, sit veritus docendi maluisset et. Nam id semper efficiendi, justo salutatus concludaturque ex vel. Populo melius no usu, ut nam albucius vituperata. Dolore offendit erroribus ei vel.",
 				"/images/superman.jpg");
+		Review review4 = new Review(comedy, "Happy Gilmore",
+				"Lorem ipsum dolor sit amet, audire vituperatoribus pro ad, at facilisi pertinax nam. Sea error dicunt consectetuer ei, vel pertinax rationibus cu, primis nostrud accusamus ea has. Pri ornatus iracundia ea, decore luptatum ea pri. Quidam legendos constituto ad quo, splendide gloriatur contentiones sit ad, minim impetus cum et. Viris nonumy principes vix ne, eu mea modus quodsi.\r\n"
+						+ "\r\n"
+						+ "Admodum eligendi convenire et has, vocibus consequat sea eu. Nam nobis aliquip probatus ad, his harum aperiam instructior ad. Has mentitum comprehensam ad, suas viris clita no has. Cum ne sale nonumes. Tota novum ocurreret no pro, cum eruditi fastidii suavitate id, ex modus timeam qui.\r\n"
+						+ "\r\n"
+						+ "Quo democritum elaboraret ne, has solum inermis in. Quidam fabulas urbanitas ad his, te iudico scaevola ullamcorper sit, stet convenire cotidieque nam at. At mel malis facilisis sententiae, ei malorum maiorum sit, vidisse antiopam ex vim. Ea mea legimus pertinax persequeris. Usu posse nihil deseruisse no, ut aliquam sadipscing pro. Eam no minim denique mediocrem, dicta veniam laboramus eum id.\r\n"
+						+ "\r\n"
+						+ "Mel prima laudem at. Sed ut quodsi prompta facilisis, cum ad congue iisque, sit veritus docendi maluisset et. Nam id semper efficiendi, justo salutatus concludaturque ex vel. Populo melius no usu, ut nam albucius vituperata. Dolore offendit erroribus ei vel.",
+				"1996",
+				"Lorem ipsum dolor sit amet, audire vituperatoribus pro ad, at facilisi pertinax nam. Sea error dicunt consectetuer ei, vel pertinax rationibus cu, primis nostrud accusamus ea has. Pri ornatus iracundia ea, decore luptatum ea pri. Quidam legendos constituto ad quo, splendide gloriatur contentiones sit ad, minim impetus cum et. Viris nonumy principes vix ne, eu mea modus quodsi.\r\n"
+						+ "\r\n"
+						+ "Admodum eligendi convenire et has, vocibus consequat sea eu. Nam nobis aliquip probatus ad, his harum aperiam instructior ad. Has mentitum comprehensam ad, suas viris clita no has. Cum ne sale nonumes. Tota novum ocurreret no pro, cum eruditi fastidii suavitate id, ex modus timeam qui.\r\n"
+						+ "\r\n"
+						+ "Quo democritum elaboraret ne, has solum inermis in. Quidam fabulas urbanitas ad his, te iudico scaevola ullamcorper sit, stet convenire cotidieque nam at. At mel malis facilisis sententiae, ei malorum maiorum sit, vidisse antiopam ex vim. Ea mea legimus pertinax persequeris. Usu posse nihil deseruisse no, ut aliquam sadipscing pro. Eam no minim denique mediocrem, dicta veniam laboramus eum id.\r\n"
+						+ "\r\n"
+						+ "Mel prima laudem at. Sed ut quodsi prompta facilisis, cum ad congue iisque, sit veritus docendi maluisset et. Nam id semper efficiendi, justo salutatus concludaturque ex vel. Populo melius no usu, ut nam albucius vituperata. Dolore offendit erroribus ei vel.",
+				"/images/gilmore.jpg");
 		
 		
 		//saving review objects to review repo
 		reviewRepo.save(review1);
 		reviewRepo.save(review2);
 		reviewRepo.save(review3);
-		
-		
-		
-		
+		reviewRepo.save(review4);
+				
 
 	}
 
